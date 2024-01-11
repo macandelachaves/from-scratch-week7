7f12c6ec4ffa5cffd5f40854a89ba3ac6555f30e
macandelachaves <149009525+macandelachaves@users.noreply.github.com>
2024-01-11 20:47:24 +0000

    injection of image + other CSS changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,44 @@ function refreshWeather(response) {
   let weatherTemperature = document.querySelector("#main-temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
+  let weatherDescription = document.querySelector("#weather-description");
+  let humidityDescription = document.querySelector("#humidity");
+  let windDescription = document.querySelector("#wind");
+  let currentTime = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  console.log(response.data);
+
+  cityElement.innerHTML = response.data.city;
+  currentTime.innerHTML = formatDate(date);
+  weatherDescription.innerHTML = response.data.condition.description;
+  humidityDescription.innerHTML = `${response.data.temperature.humidity}%`;
+  windDescription.innerHTML = `${response.data.wind.speed}km/h`;
   weatherTemperature.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img
+    src= "${response.data.condition.icon_url}"
+    class="main-emoji"
+  />`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -22,3 +57,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Buenos Aires");
